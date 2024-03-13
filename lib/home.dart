c3e06564d22ed7725b5e98ea5c09e5e50553911f
renamedname <oldworkfolder@gmail.com>
2024-03-13 15:53:04 +0100

    connected the firebase

---
+++ lib/home.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage(String s, {super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? hh;
+  bool menuu = true;
+  late Scaffold s;
+  CollectionReference users = FirebaseFirestore.instance.collection('repices');
+  // users.doc('12').get().then((DocumentSnapshot documentSnapshot){print('Document data: ${documentSnapshot.data()}');});
+
+  //   void initdatabase() async{
+  //   WidgetsFlutterBinding.ensureInitialized();
+  //   await Firebase.initializeApp();
+  // }
+
+  
+
+  void _incrementCounter() {
+    setState(() {
+     
+    });
+
+  }
+
+  late Map hh2;
+  var hhh;
+
+  @override
+  Widget build(BuildContext context) {
+  users.doc('12').get().then((DocumentSnapshot documentSnapshot){print('Document data: ${documentSnapshot.data()}');
+  hhh = documentSnapshot.data();
+  print(hhh['sq']);
+  });
+   s = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 420,
+              height: 480,
+              child:  ListView.builder(
+      itemCount: 10,
+      itemBuilder: (BuildContext context, int index) {
+        return SizedBox(
+          height:150 ,
+          child: ElevatedButton(
+            
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(1.0), 
+    ),
+  ),
+            onPressed: () {print('Button $index pressed');},
+            child:  Card(
+              child: Text(hhh['sq'],
+              selectionColor: Colors.black,
+              ),
+            
+
+            ),
+          ),
+        );
+      },
+    ),),
+          ],
+        ),
+      ),
+      
+bottomNavigationBar: BottomAppBar(
+  color: Theme.of(context).colorScheme.primaryContainer, 
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FloatingActionButton(
+            onPressed: (){
+              //initdatabase();
+              FirebaseFirestore.instance.collection("name").add({'item':"sdad"});
+            },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+      ),
+
+      SizedBox(width: 50),
+      FloatingActionButton(
+        onPressed: (){setState(() {menuu = false;}); },
+        tooltip: 'Decrement',
+        child: const Icon(Icons.home),
+      ),
+
+      SizedBox(width: 50),
+      FloatingActionButton(
+        onPressed: (){setState(() {menuu = true;});},
+        tooltip: 'Dement',
+        child: const Icon(Icons.menu),
+      )
+    ],
+  ),
+),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, // Устанавливаем положение плавающих кнопок
+    );
+
+
+
+
+
+
+
+if (menuu == true){
+    s = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+      ),
+      body: Center(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'ilosc',
+            ),
+            Text(
+              '(^◕ᴥ◕^)    ฅ^•ﻌ•^ฅ',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+    
+      SizedBox(
+        width: 300,
+       child: FloatingActionButton( 
+        onPressed: (){},
+        tooltip: 'Dement',
+        child: const Icon(Icons.abc_outlined),  
+    ),
+    ),
+
+
+      SizedBox(
+        width: 300,
+       child: FloatingActionButton( 
+        onPressed: (){},
+        tooltip: 'Dement',
+        child: const Icon(Icons.abc),  
+    ),
+    ),
+      
+      SizedBox(
+        width: 300,
+        child: FloatingActionButton(
+          onPressed: (){Navigator.pushNamedAndRemoveUntil(context, "/menu", (route) => false);},
+          tooltip: 'Dement',
+          child: const Icon(Icons.ac_unit),
+        ),
+      ),
+      SizedBox(
+        width: 300,
+        child: FloatingActionButton(
+          onPressed: (){Navigator.pushNamedAndRemoveUntil(context, "/menu", (route) => false);},
+          tooltip: 'Dement',
+          child: const Icon(Icons.access_time),
+        ),
+      )
+          ],
+        ),
+      ),
+      
+bottomNavigationBar: BottomAppBar(
+  color: Theme.of(context).colorScheme.primaryContainer, // Используйте цвет кнопок из темы
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      SizedBox(width: 50),
+      FloatingActionButton(
+        onPressed: (){setState(() {menuu = false;}); },
+        tooltip: 'Decrement',
+        child: const Icon(Icons.home),
+      ),
+
+      SizedBox(width: 50),
+      FloatingActionButton(
+        onPressed: (){setState(() {menuu = true;});},
+        tooltip: 'Dement',
+        child: const Icon(Icons.menu),
+      )
+    ],
+  ),
+),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, 
+    );
+
+
+    }
+
+  return s;
+
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:recipes/home.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // CollectionReference users = FirebaseFirestore.instance.collection('repices');
+  // users.doc('12').get().then((DocumentSnapshot documentSnapshot){print('Document data: ${documentSnapshot.data()}');});
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 150, 52)).copyWith(background: const Color.fromARGB(255, 32, 32, 32)),
+          primaryColor: Color.fromARGB(255, 255, 130, 58),
+          textTheme: TextTheme(
+          headlineMedium: TextStyle(color: Color.fromARGB(255, 31, 31, 31)),
+          bodyMedium: TextStyle(color: const Color.fromARGB(255, 38, 37, 37)),),
+              
+      ),
+   // builder: (context, child) {
+  //  return Navigator(
+     // onGenerateRoute: (settings) {
+       // return PageRouteBuilder(
+          //pageBuilder: (context, Animation, secondaryAnimation) => child!,
+          //transitionDuration: Duration(seconds: 0), // Нулевая продолжительность анимации
+         // transitionsBuilder: (context, animation, secondaryAnimation, child) {
+           // return child;
+         // },
+      //  );
+    //  },
+  //  );
+ // },
+
+    initialRoute: "/",
+    routes: {
+      "/":(context) => MyHomePage("flutter", title: 'fluter',),
+      
+      
+    },
+
+      
+    );
+  }
+}
+
